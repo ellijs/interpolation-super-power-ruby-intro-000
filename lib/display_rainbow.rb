3e69ffe2fc3a14e87cf0386b7e6af3c089a775f7
ji sun lee <ellijs@hotmail.com>
2021-03-16 20:14:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,8 @@
 #display_rainbow
 def display_rainbow(
-  color = ["red", "orange", "yellow", "green", "blue", 'indigo", "violet"])
-  letter = ["R", "O", "Y", "G", "B", "I", "V"]
-  puts "#{letter}: #{color}"
+  color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  letter = ["R", "O", "Y", "G", "B", "I", "V"])
+  puts "#{letter[x]}: #{color[x]}"
 end
 
 display_rainbow(color[0])
