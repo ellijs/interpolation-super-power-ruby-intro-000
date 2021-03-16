894cd8f0f3c0f33b96424d5147ec3cc4952d73a1
ji sun lee <ellijs@hotmail.com>
2021-03-16 20:01:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,9 +7,3 @@ def display_rainbow(
 end
 
 display_rainbow(letter[0], color[0])
-display_rainbow(letter[1], color[1])
-display_rainbow(letter[2], color[2])
-display_rainbow(letter[3], color[3])
-display_rainbow(letter[4], color[4])
-display_rainbow(letter[5], color[5])
-display_rainbow(letter[6], color[6])
